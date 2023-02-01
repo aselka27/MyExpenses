84c62ccf9623658ff831ed0772a6ee4e1a3bcab8
aselka27 <asjumalieva@gmail.com>
2023-02-01 21:15:24 +0600

    Initial Commit

---
+++ MyExpenses/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MyExpenses
+//
+//  Created by саргашкаева on 1.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyExpenses/MyExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyExpensesApp.swift
+//  MyExpenses
+//
+//  Created by саргашкаева on 1.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
