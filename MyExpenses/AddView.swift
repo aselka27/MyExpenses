58a962c9b8da4153ddf8c972982b32f759e3eea1
aselka27 <asjumalieva@gmail.com>
2023-02-03 10:15:00 +0600

    Added save button action

---
+++ MyExpenses/AddView.swift
@@ -0,0 +1,47 @@
+//
+//  AddView.swift
+//  MyExpenses
+//
+//  Created by саргашкаева on 1.02.2023.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = ""
+    @State private var amount = ""
+    
+    let types = ["Business", "Personal"]
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add")
+            .navigationBarItems(trailing: Button("Save") {
+                if let actualAmount = Int(self.amount) {
+                    let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
+                    self.expenses.items.append(item)
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+            })
+        }
+    }
+}
+//    
+//    struct AddView_Previews: PreviewProvider {
+//        static var previews: some View {
+//            AddView(expenses: Expenses())
+//        }
+//    }
+
--- MyExpenses/ContentView.swift
+++ MyExpenses/ContentView.swift
@@ -7,14 +7,70 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct ExpenseItem: Identifiable, Codable {
+    let id = UUID() // universally unic identifier
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "items")
+            }
         }
+    }
+    
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var showingAddExpense = false
+    
+    @ObservedObject var expenses = Expenses()
+   
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        Text("$\(item.amount)")
+                    }
+                }
+                .onDelete(perform: removeItems(as:))
+            }
+            .navigationBarTitle("My expenses")
+            .navigationBarItems(trailing: Button (action: {
+                self.showingAddExpense = true
+            }) {
+                Image(systemName: "plus")
+            }
+            )
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: self.expenses)
+            }
+        }
+    }
+    func removeItems(as offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
